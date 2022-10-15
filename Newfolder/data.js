57749a9060da7ac62ab9c28cc073fdbb5dd1fca5
Hanzala Bin Omar <91691179+BabriWala@users.noreply.github.com>
2022-10-15 19:18:17 +0600

    0

--- Newfolder/data.js
+++ Newfolder/data.js
@@ -54,10 +54,10 @@ document.querySelector('.convertBtn').addEventListener('click', () => {
 'UÖ': 'Uª',
 'Ww়': 'wo',
 'k…': 'k„',
-    
+ 'I‡V': 'I‡V',   
     
   }
-  str = str.replace(/h়|h‡়|m…|K…|W়|h‡়v|–|Üy|c…|e…|W‡়|wN&b|›`Ö|dÖ|`¨y|-|¯§…|oy|Uy|Ky|Zy|Zª¨|g‡v|c‡v|i‡v|M‡v|j‡v|b‡v|Z…|g…|Z¨y|q‡v|dy|f‡v|hw়|N‡v|¶y|b…|i‡v|`…|h‡v|m‡v|nÖ|c‡v|Z‡v|óÖ|b‡v|e‡v|k‡v|UÖ|Ww়|k„/gi, function (matched) {
+  str = str.replace(/h়|h‡়|m…|K…|W়|h‡়v|–|Üy|c…|e…|W‡়|wN&b|›`Ö|dÖ|`¨y|-|¯§…|oy|Uy|Ky|Zy|Zª¨|g‡v|c‡v|i‡v|M‡v|j‡v|b‡v|Z…|g…|Z¨y|q‡v|dy|f‡v|hw়|N‡v|¶y|b…|i‡v|`…|h‡v|m‡v|nÖ|c‡v|Z‡v|óÖ|b‡v|e‡v|k‡v|UÖ|Ww়|k„|I‡V/gi, function (matched) {
     return mapObj6[matched];
   });
 
